ab582e030af0e2a4c707e1a1eb3c6bfc7bb6cd25
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-04-10 09:53:56 +0300

    add routes for discipline

--- app/useDB/disciplines_db.py
+++ app/useDB/disciplines_db.py
@@ -4,3 +4,16 @@ from app.useDB.tools import quarry
 def all_disciplines():
     disciplines = quarry.call("select * from discipline", commit=False, fetchall=True)
     return disciplines
+
+
+def create_discipline(*args):
+    quarry.call("insert into discipline(name_dis) "
+                "values (%s)", *args, commit=True, fetchall=False)
+
+
+def inf_about_discipline(num_dis):
+    inf = quarry.call("select * from discipline where num_dis = %s",
+                      [num_dis],
+                      commit=False, fetchall=False)
+
+    return inf
--- disciplines/disciplines.py
+++ disciplines/disciplines.py
@@ -19,4 +19,20 @@ def all_disciplines():
 
         return jsonify(json_disciplines)
 
-    elif request.method == "POST"
+    elif request.method == "POST":
+        name_dis = request.json["name_dis"]
+
+        disciplines_db.create_discipline([name_dis])
+
+        return jsonify(f"discipline {name_dis} is add")
+
+
+@disciplines.route("/<int:num_dis>", methods=["GET", "DELETE"])
+def inf_about_discipline(num_dis):
+    if request.method == "GET":
+        inf = disciplines_db.inf_about_discipline(num_dis)
+
+        return jsonify({
+            "num_dis": inf[0],
+            "name_dis": inf[1]
+        })
